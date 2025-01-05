4b9ca829bc6132eec0f0dca5df98e6275cec3b8e
Keshavshetty707 <mkeshav9241217275@gmail.com>
2025-01-05 08:22:53 +0530

    created fucntion to check the inserted value

--- hangman_Game.py
+++ hangman_Game.py
@@ -5,7 +5,15 @@ def choose_word():
     words = ["python", "java", "javascript", "kotlin", "swift", "ruby", "csharp", "go", "typescript", "php","keshav","hangman"]
     return random.choice(words).upper()
 
-
+#it return the inserted letter if the given input is correct otherwise it return dash( _ )
+def display_word(word, guessed_letters):
+    displayed_word = ""
+    for letter in word:
+        if letter in guessed_letters:
+            displayed_word += letter + " "
+        else:
+            displayed_word += "_ "
+    return displayed_word.strip()
 
 def hangman():
     word_to_guess = choose_word()
@@ -14,3 +22,7 @@ def hangman():
     max_guesses = 6  
 
     print("Welcome to Hangman!")
+    print(display_word(word_to_guess, guessed_letters))
+
+    while "_" in display_word(word_to_guess, guessed_letters) and incorrect_guesses < max_guesses:
+        guess = input("Guess a letter: ").upper()
