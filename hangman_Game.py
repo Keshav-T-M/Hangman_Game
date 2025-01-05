31d7d780cb799c5e4ca12ae5564f9cb92d9aa749
Keshavshetty707 <mkeshav9241217275@gmail.com>
2025-01-05 08:21:13 +0530

    created some variables required

--- hangman_Game.py
+++ hangman_Game.py
@@ -4,3 +4,13 @@ import random
 def choose_word():
     words = ["python", "java", "javascript", "kotlin", "swift", "ruby", "csharp", "go", "typescript", "php","keshav","hangman"]
     return random.choice(words).upper()
+
+
+
+def hangman():
+    word_to_guess = choose_word()
+    guessed_letters = []
+    incorrect_guesses = 0
+    max_guesses = 6  
+
+    print("Welcome to Hangman!")
